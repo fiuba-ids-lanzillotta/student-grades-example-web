bbe956845513aa9584294ec05821f7a1147b83db
Chaves, Leonel Abel <lechaves@santandertecnologia.com.ar>
2026-05-20 19:28:31 -0300

    fix: point API_BASE_URL to the /student_grades_api prefix exposed by the backend

--- student_grades_example_web/constants.py
+++ student_grades_example_web/constants.py
@@ -1,5 +1,5 @@
 # URL base de la API del backend (student-grades-example-api)
-API_BASE_URL = 'http://localhost:5000'
+API_BASE_URL = 'http://localhost:5000/student_grades_api'
 
 # Validaciones para el formulario de nueva nota
 NOTA_MIN = 1
